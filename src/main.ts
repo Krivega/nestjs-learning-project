14786a80ac89aca19d8511a866a15656b525ffa2
Krivega <mr.krivega@gmail.com>
2026-01-11 20:40:52 +0300

    feat: add cookie-parser and ncsrf middleware for enhanced security in main application

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { nestCsrf } from 'ncsrf';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 import { AppModule } from '@/app.module';
@@ -9,6 +11,8 @@ async function bootstrap() {
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.use(helmet());
+  app.use(cookieParser());
+  app.use(nestCsrf());
 
   await app.listen(process.env.PORT ?? 3000);
 }
