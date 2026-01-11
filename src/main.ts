74224b162e567d4ea13689aa1e4aab74c0b75dd2
Krivega <mr.krivega@gmail.com>
2026-01-11 20:37:40 +0300

    feat: add helmet middleware for enhanced security in main application

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 import { AppModule } from '@/app.module';
@@ -7,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.use(helmet());
 
   await app.listen(process.env.PORT ?? 3000);
 }
